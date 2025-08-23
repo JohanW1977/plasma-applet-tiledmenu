39b6e54d55a8f6ff13b7b7f99e4d7f36227eb50a
JohanW1977 <101186621+JohanW1977@users.noreply.github.com>
2025-08-23 10:18:28 +0200

    Update move.sh

--- move.sh
+++ move.sh
@@ -1,3 +1,3 @@
 @ECHO OFF
-cp -fr package/* ~/.local/share/plasma/plasmoids/com.github.tryllian.tiledmenu &
+cp -fr package/* ~/.local/share/plasma/plasmoids/com.github.JohanW1977.tiledmenu &
 plasmashell --replace &
